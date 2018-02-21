0bf85d6d77101f097b15a1243a89a27214f515be
tammy le <tammyle@tammys-MBP.attlocal.net>
2018-02-20 16:58:00 -0800

    finish result layout

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,28 @@
   $(document).ready(function () {
         $('#questionDisplay').hide();
         $('#answerDisplay').hide();
+        $('#resultBox').hide();
 
         $('#start').on('click', function () {
         $('#box').hide();
-        $('body').css('background-image', 'url("assets/images/answerBg3.jpg")');
-        $("body").css({"background-position":"center top"})
+        $('body').css('background-image', 'url("assets/images/answerBg.jpg")');
+        $("body").css({"background-position":"center center"})
         $('#questionDisplay').delay(1200).animate({height: 'toggle', opacity: '1'}, 1000);
 
         $('.multiChoice').on('click', function () {
             $('#questionDisplay').hide();
             $('#answerDisplay').animate({height: 'toggle', opacity: '1'}, 1000);
-        })
+        });
+
+        $('#image-holder').on('click', function () {
+            $('#questionDisplay').hide();
+            $('#answerDisplay').hide();
+            $('body').css('background-image', 'url("assets/images/halong1.jpg")');
+            // $("body").css({"background-position":"right center"});
+            $('#resultBox').animate({width: 'toggle'}, 1000);
+            
+        });
+        
     });
+    
 });
